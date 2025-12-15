22a4674fca20a86a60075257ad763e431faea0b4
lee <lee0xb1t@qq.com>
2025-12-15 21:11:14 +0800

    use tokio time

--- src/bin/native_thread.rs
+++ src/bin/native_thread.rs
@@ -1,18 +1,16 @@
 #![allow(unused)]
 
-use std::{
-    thread::{self},
-    time::Duration,
-};
+use std::thread::{self};
 
 use tokio::task::JoinHandle;
+use tokio::time::Duration;
 
 #[tokio::main]
 async fn main() {
     let mut handles: Vec<JoinHandle<()>> = vec![];
     for i in 0..1000000 {
         let h = tokio::spawn(async move {
-            thread::sleep(Duration::from_millis(1000));
+            tokio::time::sleep(Duration::from_millis(1000));
             println!("{i}");
         });
         handles.push(h);
